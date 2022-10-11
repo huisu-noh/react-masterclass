69a55c0cdde1ff39834a6fbdc36762f2bbcad1d0
huisu <huisu0429@naver.com>
2022-10-11 17:11:09 +0900

    3.9 React Query part One

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -43,6 +43,7 @@ const OverviewItem = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 33%;
   span:first-child {
     font-size: 10px;
     font-weight: 400;
@@ -72,6 +73,7 @@ const Tab = styled.span<{ isActive: boolean }>`
   color: ${(props) =>
     props.isActive ? props.theme.accentColor : props.theme.textColor};
   a {
+    padding: 7px, 0px;
     display: block;
   }
 `;
@@ -140,10 +142,10 @@ interface PriceData {
 }
 
 function Coin() {
-  const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
 
+  const [loading, setLoading] = useState(true);
   const [info, setInfo] = useState<InfoData>();
   const [priceInfo, setPriceInfo] = useState<PriceData>();
 
