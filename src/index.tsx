88739953649b389586218086d2a606c18de41036
huisu <huisu0429@naver.com>
2022-11-08 14:18:07 +0900

    3.6 Themes

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
-import { theme } from './theme';
-
-const queryClient = new QueryClient();
+import { darkTheme, lightTheme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
